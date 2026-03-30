6d0b35430b653da1eed00e51d18ffbbdbbd810e9
immu.salmi@gmail.com <immu.salmi@gmail.com>
2026-03-30 10:07:28 +0000

    Created Form1.RowTemplate1; Edited settings and Form1

--- client_code/Form1/RowTemplate1/__init__.py
+++ client_code/Form1/RowTemplate1/__init__.py
@@ -1,11 +1,10 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import RowTemplate1Template
 from anvil import *
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-
-class Form1(Form1Template):
+class RowTemplate1(RowTemplate1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
