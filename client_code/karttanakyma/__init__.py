6fd1b9821f348ce0c132790838d48264f33312eb
anvil.elusive505@passinbox.com <anvil.elusive505@passinbox.com>
2026-04-01 09:23:34 +0000

    Edited karttanakyma

--- client_code/karttanakyma/__init__.py
+++ client_code/karttanakyma/__init__.py
@@ -5,5 +5,24 @@ class karttanakyma(karttanakymaTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    
+    self.map.center = GoogleMap.LatLng(61.055441, 28.1889914) # Lappeenrannan keskusta
+    marker = GoogleMap.Marker(
+      animation=GoogleMap.Animation.DROP,
+      position=GoogleMap.LatLng(61.0664555, 28.090938)
+    )
+    marker.add_event_handler("click", marker_click)
+    self.map.add_component(marker) # Lisää merkin Lutin parkkipaikalle
+    # Kartan dokumentaatio:
+    # https://anvil.works/docs/components/standard-components/maps
 
     # Any code you write here will run before the form opens.
+
+  @handle("map", "bounds_changed")
+  def map_bounds_changed(self, **event_args):
+    """This method is called when the viewport bounds have changed."""
+    pass
+
+def marker_click(sender, **event_args):
+  i =GoogleMap.InfoWindow(content=Label(text="Esimerkkikatu tai jtn"))
+  i.open(map, sender)
