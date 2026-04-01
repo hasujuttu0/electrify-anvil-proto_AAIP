d5346240fd8e876907080d2fa8b0da801a1749e4
anvil.elusive505@passinbox.com <anvil.elusive505@passinbox.com>
2026-04-01 08:37:20 +0000

    Edited karttanakyma

--- client_code/karttanakyma/__init__.py
+++ client_code/karttanakyma/__init__.py
@@ -7,3 +7,8 @@ class karttanakyma(karttanakymaTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  @handle("map", "bounds_changed")
+  def map_bounds_changed(self, **event_args):
+    """This method is called when the viewport bounds have changed."""
+    pass
