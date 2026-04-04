b936183ebca78a52ae4726b8e7fe2b997af325e5
leevinevalainenn@gmail.com <leevinevalainenn@gmail.com>
2026-04-04 07:32:34 +0000

    Created Kirjautumissivu; Edited settings and Form1; Deleted Form1.RowTemplate1

--- client_code/Kirjautumissivu/__init__.py
+++ client_code/Kirjautumissivu/__init__.py
@@ -1,10 +1,11 @@
-from ._anvil_designer import RowTemplate1Template
+from ._anvil_designer import KirjautumissivuTemplate
 from anvil import *
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class RowTemplate1(RowTemplate1Template):
+
+class Kirjautumissivu(KirjautumissivuTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
